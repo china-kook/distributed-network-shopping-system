00718932c8527b1741f1d86825180c266c0dc528
china-kook <935542673@qq.com>
2018-06-02 22:55:36 +0800

    FreeMarker 整合 SpringMVC 并测试

---
+++ e3-item-web/src/main/java/cn/e3mall/item/controller/HtmlGenController.java
@@ -0,0 +1,48 @@
+package cn.e3mall.item.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.view.freemarker.FreeMarkerConfigurer;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Resource;
+
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+
+/**
+ * 生成静态页面测试Controller
+ * Title: HtmlGenController
+ * version 1.0
+ */
+@Controller
+public class HtmlGenController {
+
+    @Resource
+    private FreeMarkerConfigurer freeMarkerConfigurer;
+
+    @RequestMapping("/genhtml")
+    @ResponseBody
+    public String genHtml() throws Exception {
+        Configuration configuration = freeMarkerConfigurer.getConfiguration();
+        //加载模板对象
+        Template template = configuration.getTemplate("hello.ftl");
+        //创建一个数据集
+        Map data = new HashMap<>();
+        data.put("hello", 123456);
+        //指定文件输出的路径及文件名
+        Writer out = new FileWriter(new File("D:\\ikook\\Desktop\\hell2.html"));
+        //输出文件
+        template.process(data, out);
+        //关闭流
+        out.close();
+
+        return "OK";
+    }
+}
