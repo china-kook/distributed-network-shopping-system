aa5aca16cbdcc0c8eadd45be3f7049bd7893d191
china-kook <935542673@qq.com>
2018-06-03 16:16:27 +0800

    用户注册数据有效性验证

--- e3-sso-web/src/main/java/cn/e3mall/sso/controller/RegisterController.java
+++ e3-sso-web/src/main/java/cn/e3mall/sso/controller/RegisterController.java
@@ -1,18 +1,35 @@
 package cn.e3mall.sso.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+import cn.e3mall.common.utils.E3Result;
+import cn.e3mall.sso.service.RegisterService;
 
 /**
  * 注册功能Controller
- * Title: RegitsterController
+ * Title: RegisterController
  * version 1.0
  */
 @Controller
 public class RegisterController {
 
+    @Resource
+    private RegisterService registerService;
+
     @RequestMapping("/page/register")
     public String showRegister() {
         return "register";
+    }
+
+    @RequestMapping("/user/check/{param}/{type}")
+    @ResponseBody
+    public E3Result checkData(@PathVariable String param, @PathVariable Integer type) {
+        E3Result e3Result = registerService.checkData(param, type);
+        return e3Result;
     }
 }
